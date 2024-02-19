d4b5cb515cbb899d86459164c3034c683d68d8f8
Iyanuoluwa <iyanuoluwaoluwatade@gmail.com>
2024-02-19 09:39:59 +0100

    final-3

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -13,29 +13,29 @@ const Contact =()=>{
               up to half an hour during working hours.
             </p>
           </div>
-          <div className="sm:w-[50%] w-[100%] m-4 flex flex-col gap-3"> 
+          <div className="w-[50%] w-[100%] ml-2 flex flex-col gap-3 items-center justify-center"> 
              <div className="text-[#313531] font-Helvetica text-sm font-bold leading-5 mt-2">
               <p>Company Name</p>
-              <input className="w-[90%] h-[60px] rounded-lg bg-[#D6D3CA]"></input>
+              <input className="w-[80%] h-[60px] rounded-lg bg-[#D6D3CA]"></input>
             </div>
             <div className="text-[#313531] font-Helvetica text-sm font-bold leading-5 mt-2">
               <p>Full Name</p>
-              <input className="w-[90%] h-[60px] rounded-lg bg-[#D6D3CA]"></input>
+              <input className="w-[80%] h-[60px] rounded-lg bg-[#D6D3CA]"></input>
             </div>
             <div className="text-[#313531] font-Helvetica text-sm font-bold leading-5 mt-2">
               <p>Email Address</p>
-              <input className="w-[90%] h-[60px] rounded-lg bg-[#D6D3CA]"></input>
+              <input className="w-[80%] h-[60px] rounded-lg bg-[#D6D3CA]"></input>
             </div>
             <div className="text-[#313531] font-Helvetica text-sm font-bold leading-5 mt-2">
               <p>Phone Number</p>
-              <input className="w-[90%] h-[60px] rounded-lg bg-[#D6D3CA]"></input>
+              <input className="w-[80%] h-[60px] rounded-lg bg-[#D6D3CA]"></input>
             </div>
             <div className="text-[#313531] font-Helvetica text-sm font-bold leading-5 mt-2">
               <p>How can we help you?</p>
-              <input className="w-[90%] h-[60px] rounded-lg bg-[#D6D3CA]"></input>
+              <input className="w-[80%] h-[240px] rounded-lg bg-[#D6D3CA]"></input>
             </div>
             <div>
-             <Button className="w-[90%] rounded-lg my-2 bg-[#D8A600] text-[#FFFFFF]">Send</Button>
+             <Button className="w-[80%] p-0 rounded-lg my-2 bg-[#D8A600] text-[#FFFFFF]">Send</Button>
             </div> 
            </div>
          </div>
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ import Social from "../Asset/Social.svg"
 const Footer = () => {
   return (
     <div className="bg-[#2B8C34] w-[100%] p-2 flex flex-col">
-      <div className="flex justify-between">
+      <div className="flex flex-col sm:flex-row sm:justify-between">
         <div className="w-[363px] mt-20 mx-2 p-3">
           
           <img src={c2c_logo2} alt="" />
@@ -14,7 +14,7 @@ const Footer = () => {
           <p className=" text-[#D1B912] font-normal leading-normal text-2xl font-Helvetica">Get Started now</p>
           <div className="mt-2 w-[115.123p] h-[22px]"> <img src={Social}/></div>
         </div>
-        <div className="flex flex-row mt-[87px] justify-evenly w-[50%] gap-1">
+        <div className="flex flex-col sm:flex-row mt-2 sm:mt-[87px] justify-evenly w-[50%] gap-1">
           <div className="text-[#FCFCFC]">
             <h3 className="text-2xl font-Helvetica font-normal leading-normal ">Company</h3>
             <p  className="text-[#D3D3D3] font-Helvetica font-sm">About</p>
